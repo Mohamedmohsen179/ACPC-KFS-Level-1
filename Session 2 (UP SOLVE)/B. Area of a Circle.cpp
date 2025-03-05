3e14a5392dcfa66e92c522c4ffd75c62aa5bab66
Mohamed Mohsen <117733739+Mohamedmohsen179@users.noreply.github.com>
2025-03-05 20:44:00 +0200

    Add files via upload

---
+++ Session 2 (UP SOLVE)/B. Area of a Circle.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <iomanip> // <==============
+using namespace std;
+
+int main() {
+    double r;
+    cin >> r;
+    float pi =  3.141592653;
+    cout << fixed << setprecision(9) << pi * r * r << "\n";
+    return 0;
+}
